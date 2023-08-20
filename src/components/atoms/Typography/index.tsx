7d025dc76b607b61b0565748df8847546be0dfb1
sonncho <dungcho93@gmail.com>
2023-08-21 00:46:50 +0900

    feat: Typography Component 추가

--- src/components/atoms/Typography/index.tsx
+++ src/components/atoms/Typography/index.tsx
@@ -1,17 +1,18 @@
 'use client';
 
 import React, { ReactNode } from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { CSSPropertyTextAlign, Responsive, SystemProps } from '~/types/cssProps';
 
 import { Variant } from '~/types/theme/typography';
+import { toPropValue } from '~/utils/cssProps';
 
-interface TypographyProps {
+interface TypographyProps extends SystemProps {
   children: ReactNode;
-  variant: Variant;
-}
-
-interface TypographyStyle {
   $variant: Variant;
+  $display?: 'block' | 'inline-block' | 'inline';
+  $component?: React.ElementType;
+  $align?: Responsive<CSSPropertyTextAlign>;
 }
 
 const defaultVariantMapping = {
@@ -26,21 +27,51 @@ const defaultVariantMapping = {
   body1: 'p',
   body2: 'p',
   inherit: 'p',
-  caption: 'p',
-  button: 'p',
-  overline: 'p',
+  caption: 'span',
+  button: 'span',
+  overline: 'span',
 };
 
-// const TypographyRoot = css``;
+// const
 
-const GNTypography = styled('p')<TypographyStyle>``;
+const GNTypography = styled('p')<TypographyProps>`
+  ${({ $variant, theme }) => {
+    if ($variant && theme.typography[$variant as Variant] && ($variant as Variant)) {
+      return css`
+        color: ${theme.typography[$variant].color};
+        font-size: ${theme.typography[$variant].fontSize};
+        font-weight: ${theme.typography[$variant].fontWeight};
+        letter-spacing: ${theme.typography[$variant].letterSpacing};
+        line-height: ${theme.typography[$variant].lineHeight};
+      `;
+    }
+  }}
+  display: ${({ $display }) => $display};
+  ${(props) => toPropValue('text-align', props.$align, props.theme)}
+  ${(prop) => toPropValue('margin', prop.$m, prop.theme)};
+  ${(prop) => toPropValue('margin-top', prop.$mt, prop.theme)};
+  ${(prop) => toPropValue('margin-right', prop.$mr, prop.theme)};
+  ${(prop) => toPropValue('margin-bottom', prop.$mb, prop.theme)};
+  ${(prop) => toPropValue('margin-left', prop.$ml, prop.theme)};
+  ${(prop) => toPropValue('padding', prop.$p, prop.theme)};
+  ${(prop) => toPropValue('padding-top', prop.$pt, prop.theme)};
+  ${(prop) => toPropValue('padding-right', prop.$pr, prop.theme)};
+  ${(prop) => toPropValue('padding-bottom', prop.$pb, prop.theme)};
+  ${(prop) => toPropValue('padding-left', prop.$pl, prop.theme)};
+`;
 
 const Typography = (props: TypographyProps) => {
-  const { children, variant, ...rest } = props;
-  const variantMapping = defaultVariantMapping[variant];
+  const { children, $component, $variant, $align = 'inherit', ...rest } = props;
+  const variantMapping = defaultVariantMapping[props.$variant];
 
   return (
-    <GNTypography as={variantMapping} $variant={variant} {...rest}>
+    <GNTypography
+      as={$component || variantMapping}
+      $variant={$variant}
+      $align={$align}
+      className={`GnTypography-root GnTypography-${$variant}`}
+      {...rest}
+    >
       {children}
     </GNTypography>
   );
