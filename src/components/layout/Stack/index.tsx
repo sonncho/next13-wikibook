cfedd061d18d8759aac35c1da6df08a12970c811
sonncho <dungcho93@gmail.com>
2023-08-21 13:50:42 +0900

    feat: Layout/Stack Flex 스타일을 가지는 Stack컴포넌트 생성

---
+++ src/components/layout/Stack/index.tsx
@@ -0,0 +1,47 @@
+'use client';
+
+import React, { HTMLAttributes, ReactNode } from 'react';
+import styled from 'styled-components';
+import {
+  CSSPropertyAlignItems,
+  CSSPropertyFlexDirection,
+  CSSPropertyFlexWrap,
+  CSSPropertyJustifyContent,
+  Responsive,
+} from '~/types/cssProps';
+import { toPropValue } from '~/utils/cssProps';
+
+interface StackProps extends HTMLAttributes<HTMLDivElement> {
+  children: ReactNode;
+  divider?: ReactNode;
+  $direction?: Responsive<CSSPropertyFlexDirection>;
+  $alignItems?: Responsive<CSSPropertyAlignItems>;
+  $justifyContent?: Responsive<CSSPropertyJustifyContent>;
+  $spacing?: Responsive<number>;
+  $flexWrap?: Responsive<CSSPropertyFlexWrap>;
+  $useFlexGap?: boolean;
+}
+
+const StyledStack = styled.div<StackProps>`
+  display: flex;
+  /* gap: 12px; */
+  & > :not(style) ~ :not(style) {
+    ${(props) => !props.$useFlexGap && toPropValue('margin-left', props.$spacing, props.theme)};
+  }
+  ${(props) => props.$useFlexGap && toPropValue('gap', props.$spacing, props.theme, 'rem')}
+  ${(props) => toPropValue('align-items', props.$alignItems, props.theme)};
+  ${(props) => toPropValue('justify-content', props.$justifyContent, props.theme)};
+  ${(props) => toPropValue('flex-direction', props.$direction, props.theme)}
+  ${(props) => toPropValue('flex-wrap', props.$flexWrap, props.theme)}
+`;
+
+const Stack = (props: StackProps) => {
+  const { children, divider, ...rest } = props;
+  return (
+    <StyledStack className="GnStack-root" {...rest}>
+      {children}
+    </StyledStack>
+  );
+};
+
+export default Stack;
