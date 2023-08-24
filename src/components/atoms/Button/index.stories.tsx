76458a2f2c4307c6983f02255c25aa8780412220
sonncho <dungcho93@gmail.com>
2023-08-25 03:58:41 +0900

    docs: edit Button stoires

--- src/components/atoms/Button/index.stories.tsx
+++ src/components/atoms/Button/index.stories.tsx
@@ -1,6 +1,9 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import styled from 'styled-components';
 
 import Button from '~/components/atoms/Button';
+import Box from '~/components/layout/Box';
+import Stack from '~/components/layout/Stack';
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta = {
@@ -18,10 +21,100 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Primary: Story = {
+export const BasicButton: Story = {
   args: {
-    // primary: true,
-    children: 'Button',
     $color: 'primary',
+  },
+  render: (args) => (
+    <Stack $spacing={2} $direction={'row'}>
+      <Button $variant="text" {...args}>
+        TEXT
+      </Button>
+      <Button $variant="contained" {...args}>
+        CONTAINED
+      </Button>
+      <Button $variant="outlined" {...args}>
+        OUTLINED
+      </Button>
+    </Stack>
+  ),
+};
+
+export const Color: Story = {
+  args: {
+    $size: 'medium',
+    $variant: 'contained',
+  },
+  render: (args) => (
+    <Stack $spacing={2} $direction={'row'}>
+      <Button $color={'primary'} {...args}>
+        PRIMARY
+      </Button>
+      <Button $color={'secondary'} {...args}>
+        SECONDARY
+      </Button>
+      <Button $color={'info'} {...args}>
+        INFO
+      </Button>
+      <Button $color={'success'} {...args}>
+        SUCCESS
+      </Button>
+      <Button $color={'warning'} {...args}>
+        WARNING
+      </Button>
+      <Button $color={'error'} {...args}>
+        ERROR
+      </Button>
+    </Stack>
+  ),
+};
+
+export const Size: Story = {
+  args: {
+    $size: 'medium',
+  },
+  render: (args) => {
+    const argsObj = ['small', 'medium', 'large'];
+    const StyledBox = styled(Box)`
+      & button {
+        margin: 0.5rem;
+      }
+    `;
+
+    return (
+      <StyledBox>
+        <div>
+          {argsObj.map((value) => (
+            <Button key={`text-${value}`} {...args} $size={value as 'small' | 'medium' | 'large'}>
+              {value.toUpperCase()}
+            </Button>
+          ))}
+        </div>
+        <div>
+          {argsObj.map((value) => (
+            <Button
+              key={`outlined-${value}`}
+              {...args}
+              $variant="outlined"
+              $size={value as 'small' | 'medium' | 'large'}
+            >
+              {value.toUpperCase()}
+            </Button>
+          ))}
+        </div>
+        <div>
+          {argsObj.map((value) => (
+            <Button
+              key={`contained-${value}`}
+              {...args}
+              $variant="contained"
+              $size={value as 'small' | 'medium' | 'large'}
+            >
+              {value.toUpperCase()}
+            </Button>
+          ))}
+        </div>
+      </StyledBox>
+    );
   },
 };
