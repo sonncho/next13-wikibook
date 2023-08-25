f3daa297a67ca2d08e4ece1c336a20d6248d58e6
sonncho <dungcho93@gmail.com>
2023-08-25 18:36:52 +0900

    add: Atoms/Textarea decorators  추가

--- src/components/atoms/Textarea/index.stories.tsx
+++ src/components/atoms/Textarea/index.stories.tsx
@@ -1,5 +1,9 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import React, { useState } from 'react';
+import Button from '../Button';
+import Typography from '../Typography';
 import Textarea from '.';
+import Box from '~/components/layout/Box';
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta = {
@@ -17,9 +21,47 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const BasicInput: Story = {
+export const BasicTextarea: Story = {
   args: {
     placeholder: 'Please Enter...',
   },
   render: (args) => <Textarea {...args} />,
+};
+
+const TextareaWithDecorators = ({ ...args }) => {
+  const [text, setText] = useState('');
+  const addEmoji = (emoji: string) => () => setText(`${text}${emoji}`);
+  return (
+    <Textarea
+      value={text}
+      onChange={(e) => setText(e.target.value)}
+      placeholder="Enter Text..."
+      startDecorator={
+        <Box $display={'flex'} $gap={3}>
+          <Button $variant="outlined" $color={'secondary'} $size={'small'} onClick={addEmoji('👍')}>
+            👍
+          </Button>
+          <Button $variant="outlined" $color={'secondary'} $size={'small'} onClick={addEmoji('🏖')}>
+            🏖
+          </Button>
+          <Button $variant="outlined" $color={'secondary'} $size={'small'} onClick={addEmoji('❤️')}>
+            ❤️
+          </Button>
+        </Box>
+      }
+      endDecorator={
+        <Typography $variant={'caption'} style={{ marginLeft: 'auto' }}>
+          {text.length} character(s)
+        </Typography>
+      }
+      {...args}
+    />
+  );
+};
+
+export const Decorators: Story = {
+  args: {
+    placeholder: 'Please Enter...',
+  },
+  render: (args) => <TextareaWithDecorators {...args} />,
 };
--- src/components/atoms/Textarea/index.tsx
+++ src/components/atoms/Textarea/index.tsx
@@ -13,21 +13,54 @@ interface TextareaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
    * 최대 행수
    */
   maxRows?: number;
-  hasError?: boolean;
+  error?: boolean;
+  startDecorator?: ReactNode;
+  endDecorator?: ReactNode;
 }
 
-const StyledTextarea = styled.textarea<{ hasError?: boolean }>`
+const StyledTextareaWrapper = styled.div<{ error?: boolean }>`
+  border: 1px solid
+    ${({ theme, error }) =>
+      error ? theme.palette.error.main : `rgba(${theme.palette.customColors.main}, 0.22)`};
+  border-radius: ${({ theme }) => theme.shape.borderRadius}px;
+  padding-inline-start: 0.75rem;
+  padding-block: calc(0.5rem - 1px);
+  box-sizing: border-box;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  min-width: 300px;
+  width: 100%;
+  cursor: text;
+  &:focus-within::before {
+    --Textarea-focused: 1;
+  }
+  &::before {
+    box-sizing: border-box;
+    content: '';
+    display: block;
+    position: absolute;
+    pointer-events: none;
+    inset: 0px;
+    z-index: 1;
+    border-radius: inherit;
+    margin: calc(1px * -1);
+    box-shadow: var(--Textarea-focusedInset, inset) 0 0 0 calc(var(--Textarea-focused) * 2px)
+      ${({ theme }) => theme.palette.primary.light};
+    /* box-shadow: 1px 0 0 2px ; */
+  }
+`;
+
+const StyledTextarea = styled.textarea<{ error?: boolean }>`
   font-family: ${({ theme }) => theme.typography.fontFamily};
   font-weight: 400;
   font-size: 1rem;
   line-height: 20px;
-  color: ${({ theme }) => theme.palette.text.primary};
-  border-color: ${({ theme, hasError }) =>
-    hasError ? theme.palette.error.main : `rgba(${theme.palette.customColors.main}, 0.22)`};
-  border-radius: ${({ theme }) => theme.shape.borderRadius}px;
   box-sizing: border-box;
+  color: ${({ theme }) => theme.palette.text.primary};
+  border: 0;
   outline: none;
-  padding: ${({ theme }) => theme.spacing(2, 3)};
+  padding: 0 12px 0 0;
   width: 100%;
   resize: none;
   overflow: auto;
@@ -37,8 +70,29 @@ const StyledTextarea = styled.textarea<{ hasError?: boolean }>`
   }
 `;
 
-const Textarea = (props: TextareaProps) => {
-  const { rows = 5, minRows = 5, maxRows = 10, children, hasError, onChange, ...rest } = props;
+const StyledDecorator = styled.div`
+  display: flex;
+  color: ${({ theme }) => `rgba(${theme.palette.customColors.main}, 0.22)`};
+  cursor: initial;
+  &.GnTextarea-startDecorator {
+    margin: 0px 7px 8px -5px;
+  }
+  &.GnTextarea-endDecorator {
+    margin: 8px 7px 0px -5px;
+  }
+`;
+
+const Textarea = ({
+  children,
+  rows = 3,
+  minRows = 5,
+  maxRows = 10,
+  onChange,
+  error,
+  startDecorator,
+  endDecorator,
+  ...rest
+}: TextareaProps) => {
   const [textareaRows, setTextareaRows] = useState(Math.min(rows, minRows));
 
   console.assert(!(rows < minRows), 'Textarea: rows should be generater then minRows');
@@ -67,15 +121,26 @@ const Textarea = (props: TextareaProps) => {
   );
 
   return (
-    <StyledTextarea
-      hasError={hasError}
-      rows={textareaRows}
-      aria-label={rest.placeholder}
-      onChange={handleChange}
-      {...rest}
-    >
-      {children}
-    </StyledTextarea>
+    <StyledTextareaWrapper className={`GnTextarea-root`} error={error}>
+      {startDecorator && (
+        <StyledDecorator className="GnTextarea-startDecorator">{startDecorator}</StyledDecorator>
+      )}
+      <StyledTextarea
+        className={`GnTextarea-textarea`}
+        rows={textareaRows}
+        aria-label={rest.placeholder}
+        onChange={handleChange}
+        autoComplete="off"
+        spellCheck="false"
+        autoCorrect="off"
+        {...rest}
+      >
+        {children}
+      </StyledTextarea>
+      {endDecorator && (
+        <StyledDecorator className="GnTextarea-endDecorator">{endDecorator}</StyledDecorator>
+      )}
+    </StyledTextareaWrapper>
   );
 };
 
