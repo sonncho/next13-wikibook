1d120f29c38f19f85753266547b6516234bd0c23
sonncho <dungcho93@gmail.com>
2023-08-27 21:44:51 +0900

    docs: console에 theme객체 접근 설정

--- global.d.ts
+++ global.d.ts
@@ -1,4 +1,13 @@
+import { SystemTheme, Theme } from '~/types/theme';
+
+export {};
 declare module '*.svg' {
   const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
   export default content;
+}
+
+declare global {
+  interface Window {
+    theme: Theme;
+  }
 }
--- src/@core/themes/ThemeComponent.tsx
+++ src/@core/themes/ThemeComponent.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useEffect } from 'react';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyles from '../styles/globalStyles';
 import themeOptions from './themeOptions';
@@ -17,10 +17,26 @@ const ThemeComponent = (props: IProps) => {
   const coreTheme = themeOptions('light');
 
   let theme = createTheme(coreTheme);
-
   theme = createTheme(theme, { typography: { ...typography(theme) } });
 
-  console.log('ThemeComponent.tsx ----------', theme);
+  console.log(
+    `%c
+     ██████╗  █████╗ ███╗   ██╗ ██████╗ 
+    ██╔════╝ ██╔══██╗████╗  ██║██╔═══██╗
+    ██║  ███╗███████║██╔██╗ ██║██║   ██║
+    ██║   ██║██╔══██║██║╚██╗██║██║   ██║
+    ╚██████╔╝██║  ██║██║ ╚████║╚██████╔╝
+     ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═══╝ ╚═════╝ 
+    
+    Tip: you can access the documentation "theme" object directly in the console.
+  `,
+    'font-size:10px; color:magenta;'
+  );
+
+  useEffect(() => {
+    window.theme = theme;
+    // console.info({ theme });
+  }, [theme]);
 
   return (
     <ThemeProvider theme={theme}>
